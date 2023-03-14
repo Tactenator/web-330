9f04eece1fd42efef9ca74d2463dcfcc73521a6d
Tactenator <trevor.mclaurine@gmail.com>
2023-03-14 09:11:30 -0500

    Finished week 1

--- theme.js
+++ theme.js
@@ -1,9 +1,11 @@
 window.addEventListener("DOMContentLoaded", () => { 
 
+    const icon = document.getElementById('icon-mode');
+
     const setDefaultTheme = () => {
         const theme = localStorage.getItem('mode') || "light-theme"; 
         const iconMode = localStorage.getItem('iconMode') || "fa-toggle-off"; 
-        const iconText = localStorage.getItme('iconText') || "Light Mode"; 
+        const iconText = localStorage.getItem('iconText') || "Light Mode"; 
 
         document.body.classList.value = theme; 
         document.getElementById('icon-mode').classList.add(iconMode); 
@@ -14,5 +16,26 @@ window.addEventListener("DOMContentLoaded", () => {
         document.body.classList.value = localStorage.getItem('mode') || "light-theme";
     }
 
-    setTheme();
+    const toggleMode = (e) => {
+        let colorTheme = document.body.classList; 
+        
+        if(colorTheme.value === "light-theme"){
+            document.body.classList.toggle('light-theme')
+            document.body.classList.toggle('dark-theme')
+            document.getElementById('icon-mode').classList.toggle('fa-toggle-off');
+            document.getElementById('icon-mode').classList.toggle('fa-toggle-on');
+            document.getElementById('icon-text').innerHTML = "Dark Mode"
+        }
+        else {
+            document.body.classList.toggle('dark-theme')
+            document.body.classList.toggle('light-theme')
+            document.getElementById('icon-mode').classList.toggle('fa-toggle-off');
+            document.getElementById('icon-mode').classList.toggle('fa-toggle-on');
+            document.getElementById('icon-text').innerHTML = "Light Mode"
+        }
+    }
+
+    icon.addEventListener('click', toggleMode)
+
+    setDefaultTheme();
 })
