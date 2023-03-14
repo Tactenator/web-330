6fdddc2e8118c1b477c203b25df2cb32c58e1ac4
Tactenator <trevor.mclaurine@gmail.com>
2023-03-13 22:02:55 -0500

    Adding light/dark theme

--- theme.js
+++ theme.js
@@ -1,8 +1,18 @@
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener("DOMContentLoaded", () => { 
 
-    setTheme(); 
+    const setDefaultTheme = () => {
+        const theme = localStorage.getItem('mode') || "light-theme"; 
+        const iconMode = localStorage.getItem('iconMode') || "fa-toggle-off"; 
+        const iconText = localStorage.getItme('iconText') || "Light Mode"; 
 
-    const setTheme = () => {
-        console.log('test')
+        document.body.classList.value = theme; 
+        document.getElementById('icon-mode').classList.add(iconMode); 
+        document.getElementById('icon-text').innerHTML = iconText; 
     }
+
+    const setSelectedTheme = () => {
+        document.body.classList.value = localStorage.getItem('mode') || "light-theme";
+    }
+
+    setTheme();
 })
