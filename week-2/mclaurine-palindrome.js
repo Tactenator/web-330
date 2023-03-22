12fb0e9daf6d1f34a349ebb7c0ea9732c835907c
Trevor McLaurine <trevor.mclaurine@gmail.com>
2023-03-22 09:50:22 -0500

    week 2 finished.

--- week-2/mclaurine-palindrome.js
+++ week-2/mclaurine-palindrome.js
@@ -1,9 +1,9 @@
 /*
     ==================
-    Title: mclaurine-numbergame.js, 
+    Title: mclaurine-palindrome.js, 
     Author: Trevor McLaurine
-    Date: 1/23/2023
-    Description: A guess the number game.
+    Date: 3/22/2023
+    Description: An application that checks to see if the user input is a palindrome.
 */
 
 // This bit of code checks to see if the HTML document has loaded.
@@ -17,6 +17,7 @@ window.addEventListener("DOMContentLoaded", () => {
     const assignResults = document.getElementById('assign-results')
     let phraseValue = txtPhrase.value;
 
+    //gets today's date and collects each variable into a string
     const getTodaysDate = () => {
         let today = new Date(); 
 
@@ -24,20 +25,23 @@ window.addEventListener("DOMContentLoaded", () => {
         let month = today.getMonth() + 1; 
         let year = today.getFullYear()
 
+        //checks to see if month is less than 10 to see if a 0 is needed in front of the month number.
         month = month < 10 ? '0' + month : month;
 
         return(month + '/' + day + '/' + year)
     }
 
+    //gets the length of the user input
     const getLength = (phrase) => {
         return phrase.length; 
     }
 
+    //splits the input into an array, reverses it and then joins it back together in one, single line.
     const reverse = (phrase) => {
-        console.log(phrase)
         return phrase.split('').reverse().join(''); 
     }
 
+    //checks to see if user input is indeed a palindrome.
     const isPalindrome = (phrase) => { 
         let phraseReversed = reverse(phrase); 
         if(phraseReversed === phrase) {
@@ -46,30 +50,36 @@ window.addEventListener("DOMContentLoaded", () => {
         return false; 
     }
 
+    //updates the value of the user input each time the user types or deletes
     const updatePhrase = (e) => {
         phraseValue = e.target.value
-        getLength(phraseValue)
     }
 
     const todaysDate = getTodaysDate()
-    const header = `
-                <div>
-                    Date: ${todaysDate}<br>
-                    Original Phrase: ${phraseValue}<br>
-                    Reversed Phrase: ${reverse(phraseValue)}<br>
-                    Phrase Length: ${getLength(phraseValue)} <br>
-                </div>
-                `
 
+    // an event listener to check when the user has inputted or deleted anything in the input box. 
     inputPhrase.addEventListener('input', (e) => {
         updatePhrase(e)
     }) 
 
-    //This adds the event listener so when the user clicks the button it performs the conversion function
+    //This adds the event listener so when the user clicks the button it performs the palindrome check
     phraseButton.addEventListener('click', (e) => {
-        //This sets the value of the temperature results div to the value received from the convertTemperature function.
+        //performs the check palindrome function to see if it works. 
         let palindrome = isPalindrome(phraseValue)
+        const len = getLength(phraseValue)
+
+        //creates the header that contains the variable's values.
+        const header = `
+                <div>
+                    Date: ${todaysDate}<br>
+                    Original Phrase: ${phraseValue}<br>
+                    Reversed Phrase: ${reverse(phraseValue)}<br>
+                    Phrase Length: ${len} <br>
+                </div>
+                `
         assignResultsHeader.innerHTML = header;
+
+        //sets results based on if the user input is a palindrome or not.
         if(palindrome) {
             assignResults.innerHTML = 
             `
@@ -86,5 +96,7 @@ window.addEventListener("DOMContentLoaded", () => {
             `
         }
     })
+
+    //resets the user input
     txtPhrase.value = ''
 })
