0b88d31a4e7e42adc2e7c92b070b5d1ad5caa8c3
Tactenator <trevor.mclaurine@gmail.com>
2023-04-03 21:30:49 -0500

    adding week 4; not tested

---
+++ week-4/calorie-converter.js
@@ -0,0 +1,19 @@
+import { FoodModel } from "./food-model.js";
+
+export class CalorieConverter {
+    constructor() {}
+        data = [
+            new FoodModel(1007, 'Egg', 78), 
+            new FoodModel(1008, 'Apple', 95), 
+            new FoodModel(1009, 'Hamburger', 354), 
+            new FoodModel(1010, 'Fries', 400), 
+            new FoodModel(1011, 'Banana', 105), 
+            new FoodModel(1012, 'Soda', 150)
+    ]
+
+    find(e) {
+        return data.find(item => item.name === e)
+    }
+        
+    
+}
