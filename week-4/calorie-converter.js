146dfd4eaaa427e48627191d45d98f82f67847a9
Trevor McLaurine <trevor.mclaurine@gmail.com>
2023-04-04 13:12:17 -0500

    added week 4

--- week-4/calorie-converter.js
+++ week-4/calorie-converter.js
@@ -1,20 +1,27 @@
+/*
+    ==================
+    Title: mclaurine-calorie-converter.js, 
+    Author: Trevor McLaurine
+    Date: 4/4/2023
+    Description: Creates calories converter and uses the food model class to construct data 
+*/
+
 import { FoodModel } from "./food-model.js";
 
 export class CalorieConverter {
     constructor() {}
         data = [
-            new FoodModel(1007, 'Egg', 78), 
-            new FoodModel(1008, 'Apple', 95), 
-            new FoodModel(1009, 'Hamburger', 354), 
-            new FoodModel(1010, 'Fries', 400), 
-            new FoodModel(1011, 'Banana', 105), 
-            new FoodModel(1012, 'Soda', 150)
+            new FoodModel(1007, 'egg', 78), 
+            new FoodModel(1008, 'apple', 95), 
+            new FoodModel(1009, 'hamburger', 354), 
+            new FoodModel(1010, 'fries', 400), 
+            new FoodModel(1011, 'banana', 105), 
+            new FoodModel(1012, 'soda', 150)
     ]
 
     find(e) {
-        console.log(data)
-        return data.find(item => item.name === e)
-    }
+        return this.data.find(item => item.name === e)
+    };
         
     
 }
--- week-4/food-model.js
+++ week-4/food-model.js
@@ -1,3 +1,11 @@
+/*
+    ==================
+    Title: food-model.js, 
+    Author: Trevor McLaurine
+    Date: 4/4/2023
+    Description: Creates the food model class 
+*/
+
 export class FoodModel {
     constructor(id, name, calories) { 
         this.id = id; 
