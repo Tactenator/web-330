727d426f5dd2ddf9cf184f7b07c091f33c2e80d4
Tactenator <trevor.mclaurine@gmail.com>
2023-05-06 11:05:26 -0500

    added comments

--- week-8/http-client.js
+++ week-8/http-client.js
@@ -4,6 +4,8 @@ export class HttpClient {
     async get(url, params = "") { 
         let newUrl = new URL(url);
         let searchParams = new URLSearchParams(params)
+        // This is where the mistake was. I didn't realize I had to call the url.search separately. I though it needed to be in the parameters. 
+        //Got it fixed though!
         newUrl.search = searchParams
 
         const res = await fetch(newUrl.toString() + searchParams.toString(), {
